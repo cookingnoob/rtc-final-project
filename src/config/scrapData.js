eb9b74709126ff33136d4b33ddb3cc3e5a76e741
alan <alan@iMac-de-alan.local>
2024-04-23 16:31:02 -0600

    add scroll to puppeteet and query for images urls

--- src/config/scrapData.js
+++ src/config/scrapData.js
@@ -1,26 +1,45 @@
 import puppeteer from "puppeteer";
 
 const getDataFromWebPage = async () => {
-   console.log('getting data')
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    // page.on('console', consoleObj => console.log(consoleObj.text()));
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36');
+  // page.on('console', consoleObj => console.log(consoleObj.text()));
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36"
+  );
 
-    await page.goto('https://www.elpalaciodehierro.com/belleza/fragancias/')
+  await page.goto("https://www.elpalaciodehierro.com/belleza/fragancias/");
 
-    const result = await page.evaluate(() => {
+  await page.evaluate(async () => {
+    const distance = 100;
+    const delay = 300;
+    while (window.scrollY + window.innerHeight < document.body.scrollHeight) {
+      window.scrollBy(0, distance);
+      await new Promise((resolve) => {
+        setTimeout(resolve, delay);
+      });
+    }
+  });
 
-        const products = Array.from(document.querySelectorAll('.b-product'));
-       return products.map(product => {
-          const brandElement = product.querySelector('.b-product_tile-brand');
-        return brandElement ? brandElement.textContent.trim() : 'Brand not found';
-       })
-    })
-    console.log(result)
-    return result
-}
 
+  const result = await page.evaluate(async () => {
+    const products = Array.from(document.querySelectorAll(".b-product"));
+    return products.map((product) => {
+      const brandElement = product.querySelector(".b-product_tile-brand");
+      const brandName = brandElement
+        ? brandElement.textContent.trim()
+        : "Brand not found";
+      const srcsetAttribute =
+        product
+          .querySelector(".b-product_image source")
+          ?.getAttribute("srcset") || "Srcset not found";
 
-export {getDataFromWebPage}
+      return { brandName, srcsetAttribute };
+    });
+  });
+  console.log(result);
+  return result;
+};
+
+export { getDataFromWebPage };
